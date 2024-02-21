d6361d3470f19b1610cca1fd5f951182e08b4679
annabic <anna.bicevska@gmail.com>
2024-02-21 13:54:57 +0200

    style: prefer double quotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://annabic.github.io/',
+  site: "https://annabic.github.io/",
   integrations: [tailwind()]
 });
