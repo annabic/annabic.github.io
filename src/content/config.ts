db8a1597a68076ecde7860cef82fd9f43ca08e75
annabic <anna.bicevska@gmail.com>
2024-05-10 18:40:52 +0300

    feat: add blog section, first post, draft tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,29 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
-// layout: "../../layouts/MarkdownLayout.astro"
-// section: "Lesson 1: Iterations"
-// title: "Binary Gap"
-// description: "A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N."
-// tags: ["Iterations"]
-
-// 2. Define a `type` and `schema` for each collection
 const codilityCollection = defineCollection({
-  type: "content", // v2.5.0 and later
+  type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
     section: z.string(),
     tags: z.array(z.string()),
-    date: z.date(),
-    difficulty: z.enum(["Easy", "Medium", "Hard"])
+    difficulty: z.enum(["Easy", "Medium", "Hard"]),
+    date: z.coerce.date()
   })
 });
 
-// 3. Export a single `collections` object to register your collection(s)
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    tags: z.array(z.string()),
+    date: z.coerce.date()
+  })
+});
+
 export const collections = {
-  codility: codilityCollection
+  codility: codilityCollection,
+  blog: blogCollection
 };
